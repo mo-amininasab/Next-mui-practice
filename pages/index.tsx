ddf2ca335e7be749f968610279f989d3a9b4f513
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-10-17 18:24:22 +0330

    udpate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,60 @@
-import { NextPage } from 'next'
+import React, { useState } from 'react';
 
-interface Props {
+import { NextPage } from 'next';
 
-}
+import {
+  Button,
+  ButtonGroup,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+} from '@mui/material';
+import { Save, Delete } from '@mui/icons-material';
+
+interface Props {}
 
 const HomePage: NextPage<Props> = () => {
+  const [check, setCheck] = useState(false);
   return (
     <div>
-      
-    </div>
-  )
-}
+      <Button
+        variant="contained"
+        color="info"
+        size="small"
+        startIcon={<Save />}
+      >
+        I'm Button
+      </Button>
 
-export default HomePage
+      <ButtonGroup>
+        <Button variant="contained" startIcon={<Save />}>
+          Save
+        </Button>
+        <Button variant="contained" startIcon={<Delete />} color="error">
+          Discard
+        </Button>
+      </ButtonGroup>
+
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={check}
+            onChange={() => {
+              setCheck((prevState) => !prevState);
+            }}
+          />
+        }
+        label="Testing Checkbox"
+      />
+
+      <TextField
+        variant="filled"
+        type="email"
+        label="The Date"
+        value="test@test.com"
+      />
+    </div>
+  );
+};
+
+export default HomePage;
