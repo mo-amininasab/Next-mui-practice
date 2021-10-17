a788d07fc5fc9eba8cd5cf3c9cb0fdd7ece14861
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-10-17 18:39:21 +0330

    start again

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,15 @@
-import React, { useState } from 'react';
+import { NextPage } from 'next'
 
-import { NextPage } from 'next';
+interface Props {
 
-import {
-  Button,
-  ButtonGroup,
-  Checkbox,
-  FormControlLabel,
-  TextField,
-} from '@mui/material';
-import { Save, Delete } from '@mui/icons-material';
-
-interface Props {}
+}
 
 const HomePage: NextPage<Props> = () => {
-
-  const [check, setCheck] = useState(false);
   return (
     <div>
-      <Button
-        variant="contained"
-        color="info"
-        size="small"
-        startIcon={<Save />}
-        sx={{
-          background: 'linear-gradient(45deg, #fe6b8b, #ff8e53)',
-          border: 0,
-          borderRadius: 15,
-          color: 'white',
-          padding: '5px 30px',
-        }}
-      >
-        Styled Button
-      </Button>
-
-      <ButtonGroup>
-        <Button variant="contained" startIcon={<Save />}>
-          Save
-        </Button>
-        <Button variant="contained" startIcon={<Delete />} color="error">
-          Discard
-        </Button>
-      </ButtonGroup>
-
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={check}
-            onChange={() => {
-              setCheck((prevState) => !prevState);
-            }}
-          />
-        }
-        label="Testing Checkbox"
-      />
-
-      <TextField
-        variant="filled"
-        type="email"
-        label="The Date"
-        value="test@test.com"
-      />
+      Start Again
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
