12eb09dc615702ed12f258e8d19104adc52891a2
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-10-18 12:08:23 +0330

    form +

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,9 +1,34 @@
+import React, { useState } from 'react';
+
 import { NextPage } from 'next';
-import { Typography, Button, Container } from '@mui/material';
+import { Typography, Button, Container, TextField } from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 interface Props {}
 
 const CreatePage: NextPage<Props> = () => {
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const onSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title.trim() === '') {
+      setTitleError(true);
+    }
+
+    if (details.trim() === '') {
+      setDetailsError(true);
+    }
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
+
   return (
     <Container>
       <Typography
@@ -15,15 +40,45 @@ const CreatePage: NextPage<Props> = () => {
         Create a new note
       </Typography>
 
-      <Button
-        type="submit"
-        color="secondary"
-        variant="contained"
-        endIcon={<KeyboardArrowRightIcon />}
-        onClick={() => {}}
-      >
-        Submit
-      </Button>
+      <form onSubmit={onSubmitHandler} noValidate autoComplete="off">
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          sx={{
+            marginTop: 5,
+            marginBottom: 5,
+            display: 'block',
+          }}
+          variant="outlined"
+          label="Note Title"
+          color="secondary"
+          required
+          fullWidth
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          sx={{
+            marginBottom: 5,
+          }}
+          variant="outlined"
+          label="Details"
+          multiline
+          rows={4}
+          color="secondary"
+          required
+          fullWidth
+          error={detailsError}
+        />
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+          onClick={() => {}}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 };
